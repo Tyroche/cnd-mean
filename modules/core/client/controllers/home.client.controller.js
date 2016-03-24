7ce1c677b4d1d6cd0c4bf308ab34b48d2d6ef8b7
etkirsch <etkirsch@gmail.com>
2016-03-24 13:26:05 -0400

    Updated some assets to make this thing work better on cloudfoundry

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -1,8 +1,22 @@
 'use strict';
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-  function ($scope, Authentication) {
+angular.module('core').controller('HomeController', ['$scope', '$resource', 'Authentication',
+  function ($scope, $resource, Authentication) {
+    var vm = this;
+
     // This provides Authentication context.
     $scope.authentication = Authentication;
+
+    init();
+    function init() {
+      $resource('/api/articles').query({}, function(res) {
+        if (!res[0]) {
+          console.log('ERROR: No News found!!!');
+          return [];
+        }
+        $scope.news = res;
+        console.log(vm.news);
+      });
+    }
   }
 ]);
