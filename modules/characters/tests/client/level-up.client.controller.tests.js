7d92314a3c45a2d0d157801bd2ee283847b5d30e
Tyroche <croxxp@gmail.com>
2016-04-05 16:30:56 -0400

    Removed or fixed the rest of the tests. Also added phantomjs to the package json, which works now!

--- modules/characters/tests/client/level-up.client.controller.tests.js
+++ modules/characters/tests/client/level-up.client.controller.tests.js
@@ -36,7 +36,7 @@
     // The injector ignores leading and trailing underscores here (i.e. _$httpBackend_).
     // This allows us to inject a service but then attach it to a variable
     // with the same name as the service.
-    beforeEach(inject(function ($controller, $rootScope, _$location_, _$stateParams_, _$httpBackend_, _characterResolve_) {
+    beforeEach(inject(function ($controller, $rootScope, _$location_, _$stateParams_, _$httpBackend_) {
       // Set a new global scope
       $scope = $rootScope.$new();
 
@@ -44,11 +44,11 @@
       $stateParams = _$stateParams_;
       $httpBackend = _$httpBackend_;
       $location = _$location_;
-      characterResolve = _characterResolve_;
 
       // Initialize the Level up controller.
       LevelUpController = $controller('LevelUpController', {
-        $scope: $scope
+        $scope: $scope,
+        characterResolve: {playableClass: ['Rogue']}
       });
     }));
 
